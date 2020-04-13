2c707cc590afee83c0ca77713f53e437d47a44eb
LukeDaws <luke.daws@gmail.com>
2020-04-13 23:13:48 +1000

    First attempt code for Ordered array

--- OrderedArrayRQ.java
+++ OrderedArrayRQ.java
@@ -14,9 +14,29 @@ public class OrderedArrayRQ implements Runqueue {
 
     /**
      * Constructs empty queue
+     * 
      */
+	
+	
+	String[] proclabel_array = {null};
+	int[] vt_array = {};
+	
+	//I'm not sure if I need these here just yet but I'm gonna keep the variables there.
+	int vt_array_length = vt_array.length;
+	int proclabel_array_length = proclabel_array.length;
+	int[] new_vt_array = {};	
+	int position = (Integer) null;
+	String[] new_proclabel_array = {null};
+	
+	
     public OrderedArrayRQ() {
         // Implement Me
+    	
+    	//creates an array of 1
+    	
+    	proclabel_array = new String[1];
+		vt_array = new int[1];
+    	
 
     }  // end of OrderedArrayRQ()
 
@@ -25,53 +45,224 @@ public class OrderedArrayRQ implements Runqueue {
     public void enqueue(String procLabel, int vt) {
         // Implement me
 
-    } // end of enqueue()
+    	
+    	//will check if a the array is empty and if so place the input staight into the array.
+if (proclabel_array == null) {		
+    		
+									 
+			proclabel_array[0] = tokens[0];
+			vt_array[0] = Integer.parseInt(tokens[1]);
+							
+			//if the array already already has a variables then it will search through it.		
+		} else {
+				
+						
+			 
+			String proclabel_array_input = tokens[0];
+			
+			//this creates a new array that will contain the original array and the new input 
+			int vt_array_input = Integer.parseInt(tokens[1]);
+			new_vt_array = new int[vt_array_length + 1];
+			new_proclabel_array = new String[vt_array_length + 1];
+			
+			
+			//this searches through the array to find the a position where the VT is greater than the position before it and less than the one after it
+			for(int i = 0; i < vt_array_length - 1; i++){
+				
+				if (vt_array[i] < vt_array_input || vt_array[i]> vt_array_input )
+				position = i;
+			}
+			//this will copy the VT variable of the old array and place them in the correct position with respect to the new input.
+			for(int i = 0; i < vt_array_length +1; i++ ) {
+				if (i < position)
+					new_vt_array[i] = vt_array[i];
+				else if (i == position)
+					new_vt_array[i] = vt_array_input;
+				else
+					new_vt_array[i] = vt_array[i -1];
+			}
+			//This will place the proc labels for both the old and new variables in their respective positions
+			for(int i = 0; i < vt_array_length +1; i++ ) {
+				if (i < position)
+					new_proclabel_array[i] = proclabel_array[i];
+				else if (i == position)
+					new_proclabel_array[i] = proclabel_array_input;
+				else
+					new_proclabel_array[i] = proclabel_array[i -1];
+			}
+			
+			// this should then replace the the old array with the new array that has the new input.
+			vt_array = new_vt_array;
+			proclabel_array = new_proclabel_array;
+			vt_array_length = vt_array.length;
+			position = (Integer) null;
+			
+			}
+		}
+    	
+    	
+     // end of enqueue()
 
 
     @Override
     public String dequeue() {
         // Implement me
-
-        return ""; // placeholder,modify this
+    	//if there is nothing left in the array this should return an empty string
+    	if(proclabel_array == null) {
+    		
+    		return "";
+    	
+    	}else {
+    	
+    	//this should create a new array smaller than the original
+    	new_proclabel_array = new String[vt_array_length - 1];
+		new_vt_array = new int[vt_array_length - 1];
+		
+		//this takes the first in the queue so it can be output later
+		String output = proclabel_array[0];
+			
+		//this should loop through and copy from the old array and place it in the new array.
+		for(int i = 1; i < vt_array_length +1; i++) {
+			new_proclabel_array[i-1] = proclabel_array[i];
+			new_vt_array[i-1] = vt_array[i];
+		}
+		
+		//This will replace the old array with the new array
+		vt_array = new_vt_array;
+		proclabel_array = new_proclabel_array;
+		vt_array_length = vt_array.length;
+		
+		//this should output the dequeued process label
+        return output; 
     } // end of dequeue()
-
+   }
 
     @Override
     public boolean findProcess(String procLabel){
         // Implement me
-
-        return false; // placeholder, modify this
-    } // end of findProcess()
-
+    	
+    	//this will search through the label array
+    	for (int i = 0; i < vt_array_length + 1;i++) {
+    		//if there is a match it will return true
+    		if (proclabel_array[i] = tokens[0]) {
+    		return true;	
+    		
+    		//no match should return false
+    		}else {
+    		return false; 
+        
+    }
+  } // end of findProcess()  
+ }
 
     @Override
     public boolean removeProcess(String procLabel) {
         // Implement me
-
-        return false; // placeholder, modify this
-    } // end of removeProcess()
+    	//this will search through and try and find the the process
+    	for (int i = 0; i < vt_array_length + 1;i++) {
+      	//if it finds it then creates a new smaller array and crates a variable of the index position
+    		if (proclabel_array[i] = tokens[0]) {
+    			new_proclabel_array = new String[vt_array_length - 1];
+    			new_vt_array = new int[vt_array_length - 1];
+    			position = i;
+    			
+    		}
+    	}
+    		//if position is not null will add variables from old array to new skipping supplied index
+    	if ((Integer)position != null){
+    	for (int i = 0, k = 0; i < vt_array_length +1; i++);
+    		int i;
+    		int k;
+			if (i == position) {
+    			continue;
+    		}
+    		
+			new_proclabel_array[k++] = proclabel_array[i];
+			new_vt_array[k++] = vt_array[i];
+    		
+    		
+    	}else{   	
+        	//will return false if position is null
+        return false;
+        		}
+    	vt_array = new_vt_array;
+		proclabel_array = new_proclabel_array;
+		vt_array_length = vt_array.length;
+		position = (Integer) null;
+    	
+    	return true;
+    	}
+     // end of removeProcess()
 
 
     @Override
     public int precedingProcessTime(String procLabel) {
         // Implement me
-
-        return -1; // placeholder, modify this
+    	//this will search through and try and find the the process
+    	for (int i = 0; i < vt_array_length + 1;i++) {
+      	//if it finds it then creates a new smaller array and crates a variable of the index position
+    		if (proclabel_array[i] = tokens[0]) {
+    			new_proclabel_array = new String[vt_array_length - 1];
+    			new_vt_array = new int[vt_array_length - 1];
+    			position = i;
+    			
+    		}
+    	}
+    	if ((Integer)position !=null){
+    		//this should add all the preceding elements in the array
+    		int sum;
+			for ( int i = 0; i < position; i++) {
+    			sum += vt_array[i];
+    		}
+    		return sum;
+    	}else {
+    	
+        return -1;
+    	}
     }// end of precedingProcessTime()
 
 
     @Override
     public int succeedingProcessTime(String procLabel) {
         // Implement me
-
-        return -1; // placeholder, modify this
+    	//this will search through and try and find the the process
+    	for (int i = 0; i < vt_array_length + 1;i++) {
+      	//if it finds it then creates a new smaller array and crates a variable of the index position
+    		if (proclabel_array[i] = tokens[0]) {
+    			new_proclabel_array = new String[vt_array_length - 1];
+    			new_vt_array = new int[vt_array_length - 1];
+    			position = i;
+    			
+    		}
+    	}
+    	if ((Integer) position !=  null){
+    		//this should add all the preceding elements in the array
+    		int sum;
+			for ( int i = position +1; i < vt_array_length; i++) {
+    			sum += vt_array[i];
+    		}
+    		return sum;
+    	}else {
+    	
+        return -1;
+    	}
+       
     } // end of precedingProcessTime()
 
 
     @Override
     public void printAllProcesses(PrintWriter os) {
         //Implement me
-
+if(proclabel_array == null) {
+    		
+    		return;
+    	
+    	}else {
+    		for(i = o; i < vt_array_length; i++) {
+    			return;
+    			
+    		}
     } // end of printAllProcesses()
 
-} // end of class OrderedArrayRQ
+   }
+}// end of class OrderedArrayRQ
